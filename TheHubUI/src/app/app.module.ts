96bb9fc8458b10dccc75d1e5e7814743c6cb2db2
kirtipatel98 <kirti.bapa@gmail.com>
2020-07-23 20:03:24 -0700

    login start

--- TheHubUI/src/app/app.module.ts
+++ TheHubUI/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TheHubUI/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export interface user{
+    FirstName:string,
+    LastName:string,
+    UserName:string,
+    Password:string,
+    ConfirmPassword:string,
+    Email:string,
+    Bio:string,
+    Picture:string
+}
