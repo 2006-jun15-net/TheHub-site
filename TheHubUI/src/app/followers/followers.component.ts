808968d8a233e2ce86736d344f84110e57a091dc
arman <arman97rm@gmail.com>
2020-07-30 19:33:33 -0400

    followr

--- TheHubUI/src/app/followers/followers.component.ts
+++ TheHubUI/src/app/followers/followers.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+
+
+import { UserService} from '../user-service.service';
+import User from '../models/user';
+
 
 @Component({
   selector: 'app-followers',
@@ -7,9 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FollowersComponent implements OnInit {
 
-  constructor() { }
+  user: User | null = null;
+  UserEmail: string | undefined = '';
+  followers: user[];
+  constructor(
+    private userService: UserService,
+    public oktaAuth: OktaAuthService
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const userClaims = await this.oktaAuth.getUser();
+    if (userClaims){
+      this.UserEmail = userClaims.email;
+    }
+
+    this.getUser();
+    this.getFollowers();
+  }
+
+  getUser(): void{
+    if (this.UserEmail)
+    {
+      this.userService.getUser(this.UserEmail)
+      .then(user => {
+        this.user = user;
+      });
+    }
+  }
+
+  getFollowers(): void{
+    if(this.user){
+      this.userService.getFollowers(this.user.userId)
+      .then(followers =>{
+        this.followers = followers; 
+      });
+    }
   }
 
 }
