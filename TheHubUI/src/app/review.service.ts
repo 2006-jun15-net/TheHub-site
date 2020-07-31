71222888be6b177afafee12d64bffd7d7ec1581f
arman <arman97rm@gmail.com>
2020-07-30 22:37:10 -0400

    remove debugger

--- TheHubUI/src/app/review.service.ts
+++ TheHubUI/src/app/review.service.ts
@@ -63,7 +63,6 @@ export class ReviewService {
 
   async getFeed(id: number): Promise<Review[]>
   {
-    debugger;
     const accessToken = await this.oktaAuth.getAccessToken();
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json',
